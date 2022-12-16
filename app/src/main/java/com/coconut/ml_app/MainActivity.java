87be0d811f50e7b9903da7897021d9e8c861908c
tulip_fox <ggkdwnstj@naver.com>
2022-12-16 13:58:29 +0900

    Initial commit

---
+++ app/src/main/java/com/coconut/ml_app/MainActivity.java
@@ -0,0 +1,26 @@
+package com.coconut.ml_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import org.tensorflow.lite.Interpreter; //importing tensorflow module (animal classification)
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+//    Button bnt1 = new Button();
+//    Interpreter tflite =
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+//        Interpreter tflite = getTfliteInterpreter("converted_model.tflite");
+
+    }
+}
